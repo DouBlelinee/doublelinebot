6e0bc693b78e300d7d4337551ff2b492eba197f5
U-DOUBLELINE\Doubleline <csbackstep@gmail.com>
2016-04-24 13:40:18 +0700

    ex

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res){
   res.send('Hello World');
 })
 app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === doublelinebot) {
+  if (req.query['hub.verify_token'] === 'doublelinebot') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Error, wrong validation token');
