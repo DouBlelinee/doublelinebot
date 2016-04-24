2ff1e39462b1a9666f4677c7c9a41ac11cfca735
U-DOUBLELINE\Doubleline <csbackstep@gmail.com>
2016-04-24 14:28:06 +0700

    ex

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 app.get('/',function(req,res){
   res.send('Hello World');
