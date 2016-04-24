098ef66d414f893ea97716c04cf2814698fb5f2b
U-DOUBLELINE\Doubleline <csbackstep@gmail.com>
2016-04-24 14:44:15 +0700

    ex

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var request = require('request')
 var token = "CAAaLBQ6VwmkBAIW4XUPhO78uzFgkSrews47SyJHK1q6c5ovXpWepHPtErdP5pQzUN8Mj9N0GtQoEjM9Nqep2vFkZAI663Qm2fhlVTUIv3WzJg3c1PniiwiGJekLCJz0YjsyTGVoO2eHNVi2VEgkOXJqi3SB4l2tr4L5PnfAjJI30my8OoyfsiYaAECQMZD";
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 app.use(bodyParser.json())
 
 app.get('/',function(req,res){
@@ -27,26 +48,7 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
-function sendTextMessage(sender, text) {
-  messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
+
 app.set('port',(process.env.PORT || 5000))
 app.listen(app.get('port'),function(){
   console.log('Example app listening on port'+ app.get('port'))
